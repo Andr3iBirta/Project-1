dc323b0d44e570c83a6a6f0a7fab1da3ef057631
Andr3iBirta <150620451+Andr3iBirta@users.noreply.github.com>
2023-11-16 00:08:07 +0200

    Update business.c

--- business.c
+++ business.c
@@ -44,24 +44,49 @@ void data_report(struct Transaction *transactions, int counter, struct Date star
         printf("\nNo transactions have been recorded.\n");
         return;
     }
-    if(in_time_period(get_date(transactions, counter), start, end) == 0) {
-        printf("\nNo transactions have been found.\n");
-        return;
-    }
+    ///flag to check if no transactions have been found
+    _Bool found = 0;
+    ///stores the indexes
     int k = 0;
+    ///total money gained or spent
+    float total = 0;
+    
     printf("\n~income~\n\n");
     for(int i = counter; i >= 1; i--)
-        if(strcmp(get_type(transactions, i), "income") == 0)
-            printf("%2d)date: %02d/%02d/%04d\n   description: %s\n   amount: %.2f$\n",
-            ++k, get_date(transactions, i).day, get_date(transactions, i).month,
-            get_date(transactions, i).year, get_description(transactions, i),
-            get_amount(transactions, i));
+        if(in_time_period(get_date(transactions, i), start, end)) {
+            found = 1;
+            if(strcmp(get_type(transactions, i), "income") == 0)
+            {
+                printf("%2d)date: %02d/%02d/%04d\n   description: %s\n   amount: %.2f$\n",
+                ++k, get_date(transactions, i).day, get_date(transactions, i).month,
+                get_date(transactions, i).year, get_description(transactions, i),
+                get_amount(transactions, i));
+
+                total += get_amount(transactions, i);
+            }
+        }
+    if(k)
+        printf("\nTotal: %0.2f$\n", total);
+
+    total = 0;
     k = 0;
-    printf("\n~expense~\n\n");
+    printf("\n~expenses~\n\n");
     for(int i = counter; i >= 1; i--)
-        if(strcmp(get_type(transactions, i), "expense") == 0)
-            printf("%2d)date: %02d/%02d/%04d\n   description: %s\n   amount: %.2f$\n\n",
-            ++k, get_date(transactions, i).day, get_date(transactions, i).month,
-            get_date(transactions, i).year, get_description(transactions, i),
-            get_amount(transactions, i));
+        if(in_time_period(get_date(transactions, i), start, end)) {
+            found = 1;
+            if(strcmp(get_type(transactions, i), "expense") == 0)
+            {
+                printf("%2d)date: %02d/%02d/%04d\n   description: %s\n   amount: %.2f$\n",
+                ++k, get_date(transactions, i).day, get_date(transactions, i).month,
+                get_date(transactions, i).year, get_description(transactions, i),
+                get_amount(transactions, i));
+
+                total += get_amount(transactions, i);
+            }
+        }
+    if(k)
+        printf("\nTotal: %0.2f$\n", total);
+
+    if(!found)
+        printf("\nNo transactions have been found.\n");
 }
